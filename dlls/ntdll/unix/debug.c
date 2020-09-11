885ed51863e033e873b1683f1453da038f653d92
Haoyang Chen <chenhaoyang@uniontech.com>
2020-09-11 17:06:27 +0800

    ntdll: Improve the performance of log output.
    Logs with line breaks do not need to be copied.
    
    Signed-off-by: Haoyang Chen <chenhaoyang@uniontech.com>

--- dlls/ntdll/unix/debug.c
+++ dlls/ntdll/unix/debug.c
@@ -268,9 +268,18 @@ int __cdecl __wine_dbg_output( const char *str )
 
     if (end)
     {
-        ret += append_output( info, str, end + 1 - str );
-        __wine_dbg_write( info->output, info->out_pos );
-        info->out_pos = 0;
+        if (info->out_pos)
+        {
+            ret += append_output( info, str, end + 1 - str );
+            __wine_dbg_write( info->output, info->out_pos );
+            info->out_pos = 0;
+        }
+        else
+        {
+            /* output directly, no need cache */
+            ret = end - str + 1;
+            __wine_dbg_write( str, ret );
+        }
         str = end + 1;
     }
     if (*str) ret += append_output( info, str, strlen( str ));
