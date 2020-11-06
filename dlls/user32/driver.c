5461bd6c4e818946306283c612d745677c07b6ee
Gabriel Ivăncescu <gabrielopcode@gmail.com>
2020-11-06 18:03:30 +0200

    user32: Implement WCA_CLOAK in SetWindowCompositionAttribute.
    This implements the user32 and server-side of keeping track of cloaking,
    without actually doing the cloaking, which is done by the driver. The
    driver function SetWindowCompositionAttribute must return whether it
    supports cloaking, and whether it's a manual cloaking done by the app,
    or the shell cloaking a window.
    
    Manually cloaked windows can be activated and set to foreground. They also
    remain on the taskbar and can be activated from the taskbar too (and function
    like uncloaked windows), but are otherwise visibly hidden. WindowFromPoint
    doesn't work to retrieve them, though.
    
    Signed-off-by: Gabriel Ivăncescu <gabrielopcode@gmail.com>

--- dlls/user32/driver.c
+++ dlls/user32/driver.c
@@ -136,6 +136,7 @@ static const USER_DRIVER *load_driver(void)
         GET_USER_FUNC(SetFocus);
         GET_USER_FUNC(SetLayeredWindowAttributes);
         GET_USER_FUNC(SetParent);
+        GET_USER_FUNC(SetWindowCompositionAttribute);
         GET_USER_FUNC(SetWindowRgn);
         GET_USER_FUNC(SetWindowIcon);
         GET_USER_FUNC(SetWindowStyle);
@@ -340,6 +341,12 @@ static void CDECL nulldrv_SetParent( HWND hwnd, HWND parent, HWND old_parent )
 {
 }
 
+static DWORD CDECL nulldrv_SetWindowCompositionAttribute( HWND hwnd, DWORD attribute, void *data )
+{
+    SetLastError( ERROR_CALL_NOT_IMPLEMENTED );
+    return ~0;
+}
+
 static void CDECL nulldrv_SetWindowRgn( HWND hwnd, HRGN hrgn, BOOL redraw )
 {
 }
@@ -442,6 +449,7 @@ static USER_DRIVER null_driver =
     nulldrv_SetFocus,
     nulldrv_SetLayeredWindowAttributes,
     nulldrv_SetParent,
+    nulldrv_SetWindowCompositionAttribute,
     nulldrv_SetWindowRgn,
     nulldrv_SetWindowIcon,
     nulldrv_SetWindowStyle,
@@ -641,6 +649,7 @@ static USER_DRIVER lazy_load_driver =
     nulldrv_SetFocus,
     loaderdrv_SetLayeredWindowAttributes,
     nulldrv_SetParent,
+    nulldrv_SetWindowCompositionAttribute,
     loaderdrv_SetWindowRgn,
     nulldrv_SetWindowIcon,
     nulldrv_SetWindowStyle,
