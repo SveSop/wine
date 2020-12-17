76940a40933ad329ef90d95af5150f4d853f8d77
Rémi Bernon <rbernon@codeweavers.com>
2020-12-17 20:48:54 +0100

    ntdll: Set RLIMIT_NICE to its hard limit and inform the server.
    For thread priority support.

--- dlls/ntdll/unix/loader.c
+++ dlls/ntdll/unix/loader.c
@@ -1960,6 +1960,9 @@ void __wine_main( int argc, char *argv[], char *envp[] )
 #ifdef RLIMIT_AS
     set_max_limit( RLIMIT_AS );
 #endif
+#ifdef RLIMIT_NICE
+    set_max_limit( RLIMIT_NICE );
+#endif
 
     virtual_init();
     signal_init_early();
