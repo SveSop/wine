e387632c2ea3093a619183841cb24581e7f6bb5a
Sveinar Søpler <cybermax@dexter.no>
2021-05-24 14:03:35 +0200

    make req

--- include/wine/server_protocol.h
+++ include/wine/server_protocol.h
@@ -3109,6 +3109,8 @@ struct create_window_reply
     client_ptr_t   class_ptr;
     int            dpi;
     int            awareness;
+    int            needs_cloak;
+    char __pad_44[4];
 };
 
 
@@ -3151,6 +3153,8 @@ struct set_window_owner_reply
     struct reply_header __header;
     user_handle_t  full_owner;
     user_handle_t  prev_owner;
+    int            needs_cloak;
+    char __pad_20[4];
 };
 
 
@@ -3208,6 +3212,39 @@ struct set_window_info_reply
 #define SET_WIN_USERDATA  0x10
 #define SET_WIN_EXTRA     0x20
 #define SET_WIN_UNICODE   0x40
+
+
+
+struct get_window_cloaked_request
+{
+    struct request_header __header;
+    user_handle_t  handle;
+};
+struct get_window_cloaked_reply
+{
+    struct reply_header __header;
+    unsigned int   cloaked;
+    char __pad_12[4];
+};
+
+
+
+struct set_window_cloaked_request
+{
+    struct request_header __header;
+    user_handle_t  handle;
+    unsigned int   cloaked;
+    char __pad_20[4];
+};
+struct set_window_cloaked_reply
+{
+    struct reply_header __header;
+    unsigned int   count;
+    /* VARARG(windows,user_handles); */
+    char __pad_12[4];
+};
+#define SET_WINDOW_CLOAKED_ON     0x01
+#define SET_WINDOW_CLOAKED_SHELL  0x02
 
 
 
@@ -5797,6 +5834,8 @@ enum request
     REQ_set_window_owner,
     REQ_get_window_info,
     REQ_set_window_info,
+    REQ_get_window_cloaked,
+    REQ_set_window_cloaked,
     REQ_set_parent,
     REQ_get_window_parents,
     REQ_get_window_children,
@@ -6092,6 +6131,8 @@ union generic_request
     struct set_window_owner_request set_window_owner_request;
     struct get_window_info_request get_window_info_request;
     struct set_window_info_request set_window_info_request;
+    struct get_window_cloaked_request get_window_cloaked_request;
+    struct set_window_cloaked_request set_window_cloaked_request;
     struct set_parent_request set_parent_request;
     struct get_window_parents_request get_window_parents_request;
     struct get_window_children_request get_window_children_request;
@@ -6385,6 +6426,8 @@ union generic_reply
     struct set_window_owner_reply set_window_owner_reply;
     struct get_window_info_reply get_window_info_reply;
     struct set_window_info_reply set_window_info_reply;
+    struct get_window_cloaked_reply get_window_cloaked_reply;
+    struct set_window_cloaked_reply set_window_cloaked_reply;
     struct set_parent_reply set_parent_reply;
     struct get_window_parents_reply get_window_parents_reply;
     struct get_window_children_reply get_window_children_reply;
@@ -6538,7 +6581,7 @@ union generic_reply
 
 /* ### protocol_version begin ### */
 
-#define SERVER_PROTOCOL_VERSION 713
+#define SERVER_PROTOCOL_VERSION 714
 
 /* ### protocol_version end ### */
 
